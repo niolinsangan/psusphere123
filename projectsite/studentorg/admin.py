96e4ffd9d308676d06ef1c330ead681b7062c674
Despencer <tantanbadilla@gmail.com>
2024-11-06 13:15:32 +0800

    admin college

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -2,24 +2,37 @@ from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
 admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
-from django.contrib import admin
-from .models import Student, OrgMember
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")
+    search_fields = ("name", "college__college_name")
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
-    search_fields = ("lastname", "firstname",)
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program","college")
+    search_fields = ("lastname", "firstname", "college__college_name")
 
 @admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "get_member_program", "organization", "date_joined",)
-    search_fields = ("student__lastname", "student__firstname",)
+    list_display = ("student", "get_member_program", "organization", "date_joined")
+    search_fields = ("student__lastname", "student__firstname","")
 
     def get_member_program(self, obj):
         try:
             member = Student.objects.get(id=obj.student_id)
             return member.program
         except Student.DoesNotExist:
+            return None
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name","college")
+    search_fields = ("prog_name",)
+
+    def get_college(self, obj):
+        try:
+            college = College.objects.get(college_name = obj.college)
+            return college.college_name
+        except College.DoesNotExist:
             return None
--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -12,7 +11,7 @@ class College(BaseModel):
     college_name = models.CharField(max_length=150)
 
     def __str__(self):
-        return self.college_name
+        return self.    college_name
 
 class Program(BaseModel):
     prog_name = models.CharField(max_length=150)
@@ -21,7 +20,7 @@ class Program(BaseModel):
     def __str__(self):
         return self.prog_name
 
-class Organization(BaseModel):
+class Organization(BaseModel): 
     name = models.CharField(max_length=250)
     college = models.ForeignKey(College, null=True, blank=True, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
@@ -35,6 +34,7 @@ class Student(BaseModel):
     firstname = models.CharField(max_length=25)
     middlename = models.CharField(max_length=25, blank=True, null=True)
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    college = models.ForeignKey(College, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f"{self.lastname}, {self.firstname}"
